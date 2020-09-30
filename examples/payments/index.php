040309c12b7d9488088b83edaebfcbaf54e357f6
Samuh-El <62481894+Samuh-El@users.noreply.github.com>
2020-09-29 21:45:09 -0300

    Add files via upload

---
+++ examples/payments/index.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Ejemplo de creación de una orden de cobro, iniciando una transacción de pago
+ * Utiliza el método payment/create
+ */
+require(__DIR__ . "/../../lib/FlowApi.class.php");
+//require(__DIR__ . "/../../index.php");
+
+
+// RECIBE VALORES DEL CLIENTE
+$subject = $_REQUEST['subject'];
+	$email = $_REQUEST['email'];
+	$valor = $_REQUEST['valor'];
+
+
+// $subjectC = "Enviado desde el otro formulario";
+// $emailC = "correoXDD@gmail.com";
+// $valorC = 3000;
+
+
+try
+{
+
+//Para datos opcionales campo "optional" prepara un arreglo JSON
+$optional = array(
+	//x"rut" => "9999999-9",
+	//"otroDato" => "otroDato"
+);
+$optional = json_encode($optional);
+
+
+//Prepara el arreglo de datos
+$params = array(
+	"commerceOrder" => rand(1100,2000),
+	"subject" => $subjectC,
+	"currency" => "CLP",
+	"amount" => $valorC,
+	"email" => $emailC,
+	"paymentMethod" => 9,
+	"urlConfirmation" => Config::get("BASEURL") . "/examples/payments/confirm.php",
+	"urlReturn" => Config::get("BASEURL") ."/examples/payments/result.php",
+	"optional" => $optional
+);
+//Define el metodo a usar
+$serviceName = "payment/create";
+}
+
+catch(Exception $e)
+{
+	print_r("Error al preparar parámetros");
+}
+
+try {
+	// Instancia la clase FlowApi
+	$flowApi = new FlowApi;
+	// Ejecuta el servicio
+	$response = $flowApi->send($serviceName, $params,"POST");
+	//Prepara url para redireccionar el browser del pagador
+	$redirect = $response["url"] . "?token=" . $response["token"];
+	header("location:$redirect");
+} 
+
+catch (Exception $e) {
+	echo $e->getCode() . " - " . $e->getMessage();
+}
+
+?>
